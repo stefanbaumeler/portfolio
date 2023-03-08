7018aff7fd53ea2203913a294dedaf031fa1c78d
sbaumeler <stefan.baumeler@hotmail.com>
2023-03-08 20:12:58 +0100

    remove unused code

--- src/data/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    title: 'Stefan Baumeler',
-    subtitle: 'Web Developer',
-    socials: {
-        github: 'https://github.com/stefanbaumeler',
-        email: 'mailto:stefan.baumeler@hotmail.com',
-        linkedin: 'https://www.linkedin.com/in/stefan-baumeler-311016265/'
-    }
-}
